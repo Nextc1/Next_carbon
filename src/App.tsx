a97de5fff019345c9eeb30cb728b2f59964fd1ad
rxhul18 <rahulshah0728@gmail.com>
2025-01-30 19:50:38 +0530

    Add routing and new pages: implement Home and Dashboard components, update App structure, and enhance loading overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom"
 
+import { Home } from "./pages/Home"
+import TopBanner from "./components/custom/TopBanner"
+// import { useLoadingStore } from "./state-management/store";
+// import LoadingOverlay from "./components/custom/LoadingOverlay";
+import { Toaster } from "./components/ui/toaster";
+import Navbar from "./components/custom/Navbar";
+import { Dashboard } from "./pages/Dashboard";
 
-const App = () => {
+function App(){
+  // const { isLoading } = useLoadingStore();
+
   return (
-    <div className="text-orange-400 underline">App Initailzed</div>
+    <div className="min-h-screen">
+       {location.pathname.includes("/dashboard") ? null : <TopBanner />}
+       {location.pathname.includes("/dashboard") ? null : <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
