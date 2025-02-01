fe9748cdfd5eba54b0880da839447640b92b2597
Abhishek <mauryaabhishek1508@gmail.com>
2025-02-01 08:18:30 +0530

    Updated environment variable handling

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URI || "";;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON || "";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URI ; 
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON ; 
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     throw new Error("Missing Supabase environment variables!");
